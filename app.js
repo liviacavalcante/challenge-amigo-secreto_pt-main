7e120a18903d8b3519b3d4c5a4c81add620a0897
liviacavalcante <marialivia201589@gmail.com>
2025-03-05 15:03:21 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    let nomeAmigo = inputAmigo.value;
+
+    if (!nomeAmigo) {
+        alert("Digite o nome do amigo");
+        return;
+    }
+    amigos.push(nomeAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    atualizarLista();
+
+}
+
+
+function atualizarLista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        listaAmigos.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0) {
+        alert("Nenhum amigo adicionado")
+        return;
+    }
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `O amigo sorteado foi: ${sorteado}`;
+
+    let limparLista = document.getElementById("listaAmigos");
+    limparLista.innerHTML = "";
+}
